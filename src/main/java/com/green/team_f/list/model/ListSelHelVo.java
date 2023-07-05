c5d93c5a7749eae8504264f19a7e7c2402d17159
석민경 <smk93021@gmail.com>
2023-07-05 17:31:23 +0900

    iuser 날짜별 운동리스트출력시 운동이름 출력

--- src/main/java/com/green/team_f/list/model/ListSelHelVo.java
+++ src/main/java/com/green/team_f/list/model/ListSelHelVo.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class ListSelHelVo {
     private int ihelRecord;
-    private int ihelCate;
+    private String helName;
     private int uhKcal;
     private String ctnt;
     private String uhPic;
