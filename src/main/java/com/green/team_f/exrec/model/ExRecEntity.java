c618e47d50b51f2b3680bf479ab1632ada087f7a
석민경 <smk93021@gmail.com>
2023-07-05 14:25:11 +0900

    exRec dto수정

---
+++ src/main/java/com/green/team_f/exrec/model/ExRecEntity.java
@@ -0,0 +1,15 @@
+package com.green.team_f.exrec.model;
+
+import lombok.Data;
+
+@Data
+public class ExRecEntity {
+        private String uhPic;
+        private long ihelCate;
+        private long ical;
+        private long uhKcal;
+        private String ctnt;
+        private int time;
+        private long iuser;
+        private String recDate;
+}
