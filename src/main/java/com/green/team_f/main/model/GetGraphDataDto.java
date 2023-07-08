4fb4e51a21bbcbbdcd81b92c981dc6043ba2e0fc
석민경 <smk93021@gmail.com>
2023-07-08 12:46:50 +0900

    main페이지 그래프용 기록출력 메서드 추가

---
+++ src/main/java/com/green/team_f/main/model/GetGraphDataDto.java
@@ -0,0 +1,10 @@
+package com.green.team_f.main.model;
+
+import lombok.Data;
+
+@Data
+public class GetGraphDataDto {
+    private Long iuser;
+    private String startDate;
+    private String endDate;
+}
