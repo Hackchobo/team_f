2d12906beb019ab222aa9e3eea6ff2ecad9fe998
석민경 <smk93021@gmail.com>
2023-07-10 18:26:35 +0900

    식사기록 오류 수정(ical->iuser,recDate)

--- src/main/java/com/green/team_f/MultipartJackson2HttpMessageConverter.java
+++ src/main/java/com/green/team_f/MultipartJackson2HttpMessageConverter.java
@@ -1,4 +1,4 @@
-package com.green.team_f.exrec;
+package com.green.team_f;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
